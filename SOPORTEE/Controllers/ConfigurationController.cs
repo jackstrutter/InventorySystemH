260e76ee6258e27caa1cb02d4019ed8b85c56382
Daniel Medina <danmed9605@gmail.com>
2019-03-27 20:33:15 -0600

    Starting

---
+++ SOPORTEE/Controllers/ConfigurationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOPORTEE.Controllers
+{
+    public class ConfigurationController : Controller
+    {
+        // GET: Configuration
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult menu()
+        {
+            return View();
+        }
+    }
+}
