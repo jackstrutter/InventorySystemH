4207d9526d868206dcd93b4369a9585d9d2b6372
Lilian Jeanette Gonzalez Frias <gonzalezlj@GlobalHitss.local>
2019-04-01 18:04:56 -0600

    index de degrees

---
+++ SOPORTEE/Controllers/DegreesController.cs
@@ -0,0 +1,28 @@
+﻿using SOPORTEE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOPORTEE.Controllers
+{
+    public class DegreesController : Controller
+    {
+        // GET: Degrees
+        public ActionResult Index()
+        {
+            inventoryContext db = new inventoryContext();
+
+            return View(db.degrees.ToList());
+        }
+
+
+        public ActionResult Index2()
+        {
+            inventoryContext db = new inventoryContext();
+
+            return View(db.degrees.ToList());
+        }
+    }
+}
