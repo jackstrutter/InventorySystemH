99af7788b8b487ecf4af4eb507b9b42663747557
Lilian Jeanette Gonzalez Frias <gonzalezlj@GlobalHitss.local>
2019-04-02 17:42:14 -0600

    titulos, crear y modificar

--- SOPORTEE/Controllers/DegreesController.cs
+++ SOPORTEE/Controllers/DegreesController.cs
@@ -10,19 +10,100 @@ namespace SOPORTEE.Controllers
     public class DegreesController : Controller
     {
         // GET: Degrees
-        public ActionResult Index()
+        public ActionResult Index2(string buscar)
         {
-            inventoryContext db = new inventoryContext();
+            try
+            {
+                using (var db = new inventoryContext())
+                {
+                    IEnumerable<degrees> data = db.degrees.ToList();
+                    if (!String.IsNullOrEmpty(buscar))
+                    {
+                        data = data.Where(s => s.degrees1.Contains(buscar));
+                    }
+                    return View(data.ToList());
+                }
+            }
 
-            return View(db.degrees.ToList());
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Agregar(degrees a)
+        {
+            if(!ModelState.IsValid)
+            return View();
+
+            try
+            {
+                using (var db = new inventoryContext())
+                {
+                    db.degrees.Add(a);
+                    db.SaveChanges();
+                    return RedirectToAction("Index2");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error");
+                return View();
+            }      
         }
 
 
-        public ActionResult Index2()
+        public ActionResult Editar(int id)
         {
-            inventoryContext db = new inventoryContext();
 
-            return View(db.degrees.ToList());
+            try
+            {
+                using (var db = new inventoryContext())
+                {
+                    degrees deg = db.degrees.Find(id);
+                    return View(deg);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+              
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(degrees a)
+        {
+            try
+            {
+
+                if (!ModelState.IsValid)     // o sea si los datos estan bien, si numero es numero y asi
+                    return View();//si el modelo es validor retona la vista
+
+                using (var db = new inventoryContext())
+                {
+                    //para actualizar primero encuentro al alumno
+                    degrees deg = db.degrees.Find(a.id); //al es el alumno encontrado
+                    deg.degrees1 = a.degrees1;
+                    //agregar ultima actualizacion
+                    db.SaveChanges();
+                    return RedirectToAction("Index2");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
     }
 }
---
+++ SOPORTEE/Models/DegreesClase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SOPORTEE.Models
+{
+    public class DegreesClase
+    {
+        public int id { get; set; }
+
+        [Required]   //lo agregue yo
+        [Display(Name = "Name of the degrees ")]   //tambien puedo poner las longitudes
+        public string degrees1 { get; set; }
+    }
+
+    [MetadataType(typeof(DegreesClase))]
+    public partial class degrees
+    {
+
+    }
+    
+}
