9781d34af0246809fb86fbaf1aebc46acc226b33
Daniel Flores Medina <floresdanme@GlobalHitss.local>
2019-04-16 09:09:48 -0500

    Add project files.

---
+++ SystemInventory/Models/modelos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SystemInventory.Models
+{
+    public class modelos
+    {
+        public int id { get; set; }
+        public Nullable<int> brand_id { get; set; }
+        public string model { get; set; }
+
+        public virtual Brands Brands { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Devices> Devices { get; set; }
+    }
+}
